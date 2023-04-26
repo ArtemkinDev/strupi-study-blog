b0713c83e73d0af73ca74a61b9ae99411629be89
oleksandr artomkin <oleksandr.artomkin@p2h.com>
2023-04-26 12:14:32 +0300

    Updates

--- config/plugins.js
+++ config/plugins.js
@@ -1,9 +1,7 @@
-module.exports = {
-    // ...
-    'github-projects': {
-        enabled: true,
-        resolve: './src/plugins/github-projects'
-    },
-    // ...
-}
-
+module.exports = ({ env }) => ({
+  // ...
+  'github-projects': {
+    enabled: true,
+    resolve: './src/plugins/github-projects',
+  },
+});
